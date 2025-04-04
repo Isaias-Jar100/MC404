c106a5d8edc8a9ca542251dba88df08d8c578ef6
Isaias-Jar100 <isaiasjjna@gmail.com>
2025-04-04 04:27:53 -0300

    lab04

--- Lab04/lab04.c
+++ Lab04/lab04.c
@@ -49,9 +49,6 @@ void _start()
 #define STDIN_FD  0
 #define STDOUT_FD 1
 
-void pack(int input, int start_bit, int end_bit, int *val){
-
-}
 
 void hex_code(int val){
     char hex[11];
@@ -88,29 +85,38 @@ void str2dec(char entrada[], int saida[]) {
 int N1operação(int entrada[]){
     int N1 =0;
     N1 = entrada[0] & entrada[1];
-
+    return N1;
 }
 
 int N2operação(int entrada[]){
     int N2 = 0;
     N2 = entrada[3] | entrada[4];
+    return N2;
 }
 
 int N3operação(int entrada[]){
     int N3 = 0;
     N3 = entrada[5] ^ entrada[6];
+    return N3;
 }
 
 int N4operação(int entrada[]){
     int N4 = 0;
     N4 = ~(entrada[7] ^ entrada[8]);
+    return N4;
 }
 
 int montar_a_resposta(int n1, int n2, int n3, int n4){
+    int resultado = 0;
+    resultado |= (n1 & 0xFF);       // Bits 0 a 7
+    resultado |= (n2 & 0xFF) << 8;  // Bits 8 a 15
+    resultado |= (n4 & 0xFF) << 16; // Bits 16 a 23
+    resultado |= (n3 & 0xFF) << 24; // Bits 24 a 31
 
+    return resultado;
 }
 
-void dec2str(int entrada[], char saida[]) {
+void dec2str(int entrada[], char saida[]){
     for (int i = 0; i < 8; i++) {
         int pos = i * 6;
         if (entrada[i] < 0) {
@@ -135,10 +141,25 @@ void dec2str(int entrada[], char saida[]) {
 
 int main()
 {
-  char str[47];
-  /* Read up to 8 bytes from the standard input into the str buffer */
-  int n = read(STDIN_FD, str, 47);
-  /* Write n bytes from the str buffer to the standard output */
-  write(STDOUT_FD, str, n);
+    char str[47];
+    int dec[8];
+    /* Read up to 8 bytes from the standard input into the str buffer */
+    int n = read(STDIN_FD, str, 47);
+    /* Write n bytes from the str buffer to the standard output */
+    
+    int N1,N2,N3,N4;
+    int resposta = 0;
+
+    str2dec(str,dec);
+    N1 = N1operação(dec);
+    N2 = N2operação(dec);
+    N3 = N3operação(dec);
+    N4 = N4operação(dec);  
+
+    resposta = montar_a_resposta(N1,N2,N3,N4);
+    
+    hex_code(resposta);
+
+
   return 0;
 }
