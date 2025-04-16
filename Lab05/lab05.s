0ee8a00be91a1b7c432c325255d135911276f62c
Isaias-Jar100 <isaiasjjna@gmail.com>
2025-04-16 18:53:56 -0300

    enetendendo loops

--- Lab05/lab05.s
+++ Lab05/lab05.s
@@ -2,19 +2,26 @@
 
 _start:
     jal main
+
+exit:
     li a0, 0
     li a7, 93 # exit
     ecall
 
 
 main:
-   jal read
-
-
+    jal read
+    lb t0, 8(a1) # byte 1 da entrada ( load + 8 bites da entrada)
+    lb t1, 9(a1)    # byte 5 
+    addi t0, t0, -48 # subtraçao carteada resgitrador + numero
+    addi t1, t1, -48 # subtraçao carteada resgitrador + numero
+    li t2, 10
+    mul t0, t0, t2  #dezenha 
+    add t0, t0, t1 # valor x de b
 
    
    
-   ret
+   j exit
 
 
 read:
@@ -34,8 +41,16 @@ write:
     ret
 
 hipotenusa:
+    li t5, 10
+    li t4, 0
+
+edf:
+    addi t5, t5, 1
+    beq t5, t4,  fim
+    j edf
 
 
+fim:
 
     ret
 .bss
